70f62813fb100488b7fea24dc777b6316263a0cf
isehou <zaku47shift@gmail.com>
2024-01-12 23:56:05 +0600

    fixed bugs, added all function, finished project

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
 const searchInput = document.querySelector(".search-input");
-const list = document.querySelector(".main-gif__list");
+const list = document.querySelector(".list");
+const searchBtn = document.querySelector(".search-btn");
 
-function giphyFetch() {
-  const apiKey = "1I30QlcjptfbMRUOGz34S0hmWF30aT7N";
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+function fetchGifs(handleSearch) {
+  const API_KEY = "1I30QlcjptfbMRUOGz34S0hmWF30aT7N";
+  const count = 50;
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=${count}&q=${handleSearch}`;
 
-  const result = fetch(apiUrl)
+  list.innerHTML = "";
+
+  const fetchingUrl = fetch(URL)
     .then((res) => {
       if (!res.ok) {
-        throw new Error(`HTTP error ${res.status}`);
+        throw new Error(`HTTP failed ${res.status}`);
       } else {
         return res.json();
       }
     })
     .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.error("Fetch error:", error));
+      // console.log(data);
+      let totalData = data.data;
+      totalData.forEach((gif) => {
+        let div = document.createElement("div");
+        div.className = "item";
 
-  return result.map((result) => {});
+        let img = document.createElement("img");
+        img.classList.add("item-img");
+        img.src = gif.images.fixed_height.url;
+
+        div.appendChild(img);
+        list.appendChild(div);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+  document.querySelector(".search-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 }
 
-giphyFetch();
+function handleSearch() {
+  const searchElement = searchInput.value.trim();
+  if (searchElement) {
+    fetchGifs(searchElement);
+  } else {
+    console.log("Введите запрос для поиска");
+  }
+}
 
-// .searchInput.addEventListener()
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    searchBtn.click();
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  handleSearch();
+});
